be5f192658f61a4257e73e06101e7257a6abfa3f
joeyoungmin <quswmf45@naver.com>
2025-01-20 15:02:20 +0900

    [정리] 플레이그라운드 01 칩 리스트

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,49 +1,7 @@
-import { useState } from 'react';
-import type { ChipListType } from '@/types/chip';
-import ChipList from '@/components/chip-list';
-
 function Playground() {
-  const [chipData] = useState<ChipListType>([
-    {
-      id: 1,
-      label: '전체',
-    },
-    {
-      id: 2,
-      label: '아우터',
-    },
-    {
-      id: 3,
-      label: '바지',
-    },
-    {
-      id: 4,
-      label: '티셔츠',
-    },
-    {
-      id: 5,
-      label: '점퍼',
-    },
-    {
-      id: 6,
-      label: '원피스',
-    },
-  ]);
-
   return (
-    <div className="Playground" style={{ marginInline: 40 }}>
-      <h1>Playground</h1>
-      <div
-        style={{
-          inlineSize: 320,
-          blockSize: 640,
-          border: '4px solid rgba(0 0 0 / 10%)',
-          borderRadius: 12,
-          padding: 8,
-        }}
-      >
-        <ChipList items={chipData} />
-      </div>
+    <div className="Playground">
+      <h1>플레이그라운드</h1>
     </div>
   );
 }
